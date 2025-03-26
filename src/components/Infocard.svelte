9b95c7731a0b3e2de4681ebc687696e67e6534a1
Fermandeselmano <185094379+Fermandeselmano@users.noreply.github.com>
2025-03-26 01:22:21 +0000

    graficos no dashboard

--- src/components/Infocard.svelte
+++ src/components/Infocard.svelte
@@ -1,12 +1,104 @@
 <script lang="ts">
-    import { Card, P } from "flowbite-svelte";
+    import { Card, P, Chart } from "flowbite-svelte";
+    import { infoRows, type Cansat } from "../routes/global";
+    import { onDestroy } from "svelte";
 
     export let title: string = "Default";
     export let value: string = "0.00";
     export let href: string = "#";
+    export let y_key: keyof Cansat;
+
+    let x_data: any[] = $infoRows.map((row) => row.time.split(" ")[1]);
+    let y_data: any[] = $infoRows.map((row) => row[y_key]);
+
+    let options = {
+        chart: {
+            type: "area",
+            fontFamily: "Inter, sans-serif",
+            dropShadow: {
+                enabled: false,
+            },
+            toolbar: {
+                show: false,
+            },
+            animations: {
+                enabled: false,
+            },
+        },
+        tooltip: {
+            enabled: true,
+            x: {
+                show: false,
+            },
+        },
+        fill: {
+            type: "gradient",
+            gradient: {
+                opacityFrom: 0.55,
+                opacityTo: 0,
+                shade: "#1C64F2",
+                gradientToColors: ["#1C64F2"],
+            },
+        },
+        dataLabels: {
+            enabled: false,
+        },
+        stroke: {
+            width: 6,
+        },
+        grid: {
+            show: false,
+            strokeDashArray: 4,
+            padding: {
+                left: 2,
+                right: 2,
+                top: 0,
+            },
+        },
+        series: [
+            {
+                name: "Temperatura",
+                data: $infoRows.map((row) => row[y_key]).reverse(),
+                color: "#1A56DB",
+            },
+        ],
+        xaxis: {
+            categories: $infoRows.map((row) => row.time).reverse().map((time) => time.split(" ")[1]),
+            labels: {
+                show: false,
+            },
+            axisBorder: {
+                show: false,
+            },
+            axisTicks: {
+                show: true,
+            },
+        },
+        yaxis: {
+            show: false,
+        },
+    };
+
+    const sub = infoRows.subscribe((value) => {
+        options = {
+            ...options,
+            series: [
+                {
+                    name: y_key,
+                    data: value.map((row) => row[y_key]).reverse(),
+                    color: "#1A56DB",
+                },
+            ],
+        };
+    });
+
+    onDestroy(() => {
+        sub();
+    });
 </script>
 
 <Card class="flex flex-col align-middle justify-center gap-2" href={href}>
+    <Chart {options} class="h-full" />
     <P class="font-semibold text-4xl text-center">{value}</P>
     <P class="font-bold text-xl text-center">{title}</P>
 </Card>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,11 +5,11 @@
 </script>
 
 <div class="h-full w-full grid grid-cols-4 p-8 gap-8 justify-items-center">
-    <Infocard title="Temperatura" value={$infoRows[0].temperatura + "°C"} href="/temperatura" />
-    <Infocard title="Humidade" value={$infoRows[0].humidade + "%"} href="/humidade" />
-    <Infocard title="Pressão" value={$infoRows[0].presssao + "hPa"} href="/pressao" />
-    <Infocard title="Velocidade" value={$infoRows[0].velocidade + "m/s"} href="/velocidade" />
-    <Infocard title="Voltimetro" value={$infoRows[0].voltagem + "V"} href="/voltimetro" />
+    <Infocard title="Temperatura" value={$infoRows[0].temperatura + "°C"} href="/temperatura" y_key="temperatura" />
+    <Infocard title="Humidade" value={$infoRows[0].humidade + "%"} href="/humidade" y_key="humidade" />
+    <Infocard title="Pressão" value={$infoRows[0].presssao + "hPa"} href="/pressao" y_key="presssao" />
+    <Infocard title="Velocidade" value={$infoRows[0].velocidade + "m/s"} href="/velocidade" y_key="velocidade" />
+    <Infocard title="Voltimetro" value={$infoRows[0].voltagem + "V"} href="/voltimetro" y_key="voltagem" />
     <MapInfocard />
-    <Infocard title="Longitude" value={$infoRows[0].longitude + ""} href="/longitude" />
+    <Infocard title="Longitude" value={$infoRows[0].longitude + ""} href="/longitude" y_key="longitude" />
 </div>
